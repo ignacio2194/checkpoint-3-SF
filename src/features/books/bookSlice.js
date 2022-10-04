a01bd516aeb798eb2754421021c71d6c1f89ed62
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2022-10-03 22:20:13 -0300

    instalacion de dependencias y entorno de trabajo listo para usar

---
+++ src/features/books/bookSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState =[]
+
+export const bookSlice = createSlice({
+    name: 'book',
+    initialState,
+    reducers: {}
+})
+// export const {  } = bookSlice.actions
+export default bookSlice.reducer
