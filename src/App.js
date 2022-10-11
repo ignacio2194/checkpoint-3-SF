b8c59396bc46de7c0c06fbbdf0324808a16a1783
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2022-10-11 12:22:42 -0300

    integratig ezequiel's pr

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import BookCard from "./components/BookCard";
-import Navbar from "./components/Navbar";
 import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import CreateForm from "./components/CreateForm";
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-     <BookCard />
+      {/* Navbar here */}
+      <Routes>
+        <Route path="/home" element={<BookCard />} />
+        <Route
+          path="/:id"
+          // element={<BookDetail />}
+        />
+        <Route path="/createbook" element={<CreateForm />} />
+        <Route path="/updatebook/:id" element={<CreateForm />} />
+        <Route
+          path="/register"
+          // element={<UserRegister />}
+        />
+        <Route
+          path="/login"
+          // element={<UserLogin />}
+        />
+      </Routes>
+
+      {/* Footer here */}
     </div>
   );
 }
