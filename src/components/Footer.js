1c15e43a30e0932a2f9173c2d0cf54d52117248b
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2022-10-12 22:06:06 -0300

    added footer in  proyect

---
+++ src/components/Footer.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { AiFillLinkedin } from "react-icons/ai";
+import "../styles/Footer.css";
+import logoAvalith from '../assets/avalith_logo.png'
+const Footer = () => {
+  return (
+    <div className="footer d-flex justify-content-evenly p-3">
+      <div className="footer-container">
+        <h4>Contact us</h4>
+        <ul className=" list-unstyled">
+          <li>
+            {" "}
+            <a href="https://www.linkedin.com/in/ignaciobntz/">
+              {<AiFillLinkedin />} Ignacio Benitez
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a href="https://https://www.linkedin.com/in/francoyunes/">
+              {<AiFillLinkedin />} Franco Yunes
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a href="https://www.linkedin.com/in/foclemens/">
+              {<AiFillLinkedin />} Fernando Clemens
+            </a>
+          </li>
+          <li>
+            {" "}
+            <a href="https://www.linkedin.com/in/ezequiel-rango/">{<AiFillLinkedin />} Ezequiel Rango</a>
+          </li>
+          <li>
+            {" "}
+            <a href="https://www.linkedin.com/in/georginabosque/">{<AiFillLinkedin />} Georgina Bosque</a>
+          </li>
+        </ul>
+      </div>
+      <div className="text-footer">
+        <div> <p>Derechos reservados Avalith© skill factory Reactjs 2022</p> </div>
+       
+      </div>
+      <div className="footer_logo ">
+        <img src={logoAvalith} alt=""></img>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
