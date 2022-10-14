5c5f8dad53ec1fdb05f0cc2044aa8ca66f7d9b7c
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2022-10-14 16:10:56 -0300

    fixed mobile and desktop styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,49 +5,11 @@ import logoAvalith from '../assets/avalith_logo.png'
 const Footer = () => {
   return (
     <div className="footer d-flex justify-content-between p-3 container-fluid">
-      {/* <div className="footer-container">
-        <h4>Contact us</h4>
-        <ul className=" list-unstyled">
-          <li>
-            {" "}
-            <a href="https://www.linkedin.com/in/ignaciobntz/">
-              {<AiFillLinkedin />} Ignacio Benitez
-            </a>
-          </li>
-          <li>
-            {" "}
-            <a href="https://https://www.linkedin.com/in/francoyunes/">
-              {<AiFillLinkedin />} Franco Yunes
-            </a>
-          </li>
-          <li>
-            {" "}
-            <a href="https://www.linkedin.com/in/foclemens/">
-              {<AiFillLinkedin />} Fernando Clemens
-            </a>
-          </li>
-          <li>
-            {" "}
-            <a href="https://www.linkedin.com/in/ezequiel-rango/">{<AiFillLinkedin />} Ezequiel Rango</a>
-          </li>
-          <li>
-            {" "}
-            <a href="https://www.linkedin.com/in/georginabosque/">{<AiFillLinkedin />} Georgina Bosque</a>
-          </li>
-        </ul>
-      </div>
-      <div className="text-footer xs">
-        <div> <p>Derechos reservados Avalith© skill factory Reactjs 2022</p> </div>
-       
-      </div>
-      <div className="footer_logo ">
-        <img src={logoAvalith} alt=""></img>
-      </div> */}
       <div className="container-fluid">
         <div className="row">
-          <div className="row-4 text-white">
-          <ul className=" list-unstyled ">
-          <li>
+          <div className="col-12 col-sm-4  text-white text-center">
+          <ul className=" list-unstyled  ">
+          <li >
             {" "}
             <a href="https://www.linkedin.com/in/ignaciobntz/">
               {<AiFillLinkedin />} Ignacio Benitez
@@ -75,10 +37,10 @@ const Footer = () => {
           </li>
         </ul>
           </div>
-          <div className="row-4 text-white">
+          <div className="col-12 col-sm-4 text-white text-center ">
           <p>Derechos reservados Avalith© skill factory Reactjs 2022</p> 
           </div>
-          <div className="col-4 w-auto mx-auto">
+          <div className="col-12 col-sm-4 w-auto mx-auto    ">
           <img src={logoAvalith}/>
           </div>
         </div>
