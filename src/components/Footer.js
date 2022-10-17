23ec5ae2b2b904e2cd59f76c97b252ca39066db3
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2022-10-17 10:45:59 -0300

    :wrench: add: target and rel links footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,26 +7,26 @@ const Footer = () => {
     <div className="footerContainer">
       <div className="footerLinkContainer">
         <div className="footerLinks">
-          <a className="link" href="https://www.linkedin.com/in/georginabosque/">
+          <a className="link" href="https://www.linkedin.com/in/georginabosque/" target="_blank" rel="noreferrer">
             {<AiFillLinkedin />} Georgina Bosque
           </a>
-          <a className="link" href="https://www.linkedin.com/in/ignaciobntz/">
+          <a className="link" href="https://www.linkedin.com/in/ignaciobntz/" target="_blank" rel="noreferrer">
             {<AiFillLinkedin />} Ignacio Benitez
           </a>
         </div>
         <div className="footerLinks">
-          <a className="link" href="https://https://www.linkedin.com/in/francoyunes/">
+          <a className="link" href="https://www.linkedin.com/in/francoyunes/" target="_blank" rel="noreferrer">
             {<AiFillLinkedin />} Franco Yunes
           </a>
-          <a className="link" href="https://www.linkedin.com/in/ezequiel-rango/">
+          <a className="link" href="https://www.linkedin.com/in/ezequiel-rango/" target="_blank" rel="noreferrer">
             {<AiFillLinkedin />} Ezequiel Rango
           </a>
         </div>
         <div className="footerLinks">
-          <a className="link" href="https://www.linkedin.com/in/foclemens/">
+          <a className="link" href="https://www.linkedin.com/in/foclemens/" target="_blank" rel="noreferrer">
             {<AiFillLinkedin />} Fernando Clemens
           </a>
-          <a className="link" href="https://github.com/ignacio2194/checkpoint-3-SF/tree/main">
+          <a className="link" href="https://github.com/ignacio2194/checkpoint-3-SF/tree/main" target="_blank" rel="noreferrer">
           {<AiFillGithub />} Repositorio
           </a>
         </div>
