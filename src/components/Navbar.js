fc6bb81856a913713bfc2985051a0f2148e25485
ignacio2194 <82918800+ignacio2194@users.noreply.github.com>
2022-10-18 16:04:06 -0300

    first merge dev to main

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../styles/Navbar.css'
+import "../styles/Navbar.css";
 import logo from "../assets/bookstorelogo.png";
 import { Link } from "react-router-dom";
 
@@ -7,25 +7,51 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-sm navBarContainer">
       <div className="container-fluid">
-        <Link to="/"><img src={logo} alt="logo" width="120"></img></Link>
-        <button className="navbar-toggler border-warning bg-warning" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-         <span className="navbar-toggler-icon"></span>
+        <Link to="/">
+          <img src={logo} alt="logo" width="120"></img>
+        </Link>
+        <button
+          className="navbar-toggler border-warning bg-warning"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="offcanvas offcanvas-end bg-dark" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+        <div
+          className="offcanvas offcanvas-end bg-dark"
+          tabIndex="-1"
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+        >
           <div className="offcanvas-header">
-            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Menu</h5>
-            <button type="button" className="btn-close bg-warning" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+              Menu
+            </h5>
+            <button
+              type="button"
+              className="btn-close bg-warning"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
           </div>
           <div className="offcanvas-body">
             <ul className="navbar-dark ms-auto text-light">
               <Link to="/createbook" className="text-light ms-auto">
-                <li className="btnNavbar">Add book<span>Lets go!</span></li>
+                <li className="btnNavbar">
+                  Add book<span>Lets go!</span>
+                </li>
               </Link>
               <Link to="/login" className="text-light ms-auto">
-                <li className="btnNavbar">Login<span>Hi!</span></li>
+                <li className="btnNavbar">
+                  Login<span>Hi!</span>
+                </li>
               </Link>
               <Link to="/register" className="text-light ms-auto">
-                <li className="btnNavbar">Register<span>SingIn</span></li>
+                <li className="btnNavbar">
+                  Register<span>SingIn</span>
+                </li>
               </Link>
             </ul>
           </div>
